2cba5f57a6fbb2ca8313a44d3d44f8fc3eff6d66
Grant Kirill <webdotg.inbox@gmail.com>
2023-12-06 19:33:12 +0300

    test

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+const {login, register, current} = require('../controllers/users')
+
+/* api/user/login */
+router.post('/login', login );
+
+
+/* api/user/register */
+router.post('/register', register);
+
+
+/* api/user/current */
+router.get('/current', current);
+
+module.exports = router;
