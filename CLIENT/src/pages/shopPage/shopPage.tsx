e73026fc36e97986211d72f39df8cb075081a6b7
Grant Kirill <webdotg.inbox@gmail.com>
2023-12-11 22:35:21 +0300

    add logic open menu shop page

--- CLIENT/src/pages/shopPage/shopPage.tsx
+++ CLIENT/src/pages/shopPage/shopPage.tsx
@@ -6,6 +6,7 @@ function ShopPage() {
   const [selectedSiteSecondeType, setSelectedSiteSecondeType] = useState('');
   const [isThirdBlockVisible, setIsThirdBlockVisible] = useState(false);
   const [isSiteChecked, setIsSiteChecked] = useState(false);
+  const [isFourthBlockVisible, setIsFourthBlockVisible] = useState(false);
 
   const handleSiteTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsSiteChecked(e.target.value === 'сайты' && e.target.checked);
@@ -15,6 +16,11 @@ function ShopPage() {
     setSelectedSiteSecondeType(e.target.value);
     setIsThirdBlockVisible(true);
   };
+
+  const handleSiteTypeThirdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsFourthBlockVisible(e.target.checked);
+  };
+
 
   return (
     <>
@@ -70,13 +76,32 @@ function ShopPage() {
               type='radio'
               name='siteTypeThird'
               value='индивидуальный'
+              onChange={handleSiteTypeThirdChange}
             />
             <label>шаблон</label>
             <input
               type='radio'
               name='siteTypeThird'
               value='шаблон'
+              onChange={handleSiteTypeThirdChange}
             />
+          </div>
+        )}
+
+        {isFourthBlockVisible && (
+          <div className={style.site_fourth_blok_wrapper}>
+            <label>дизайн</label>
+            <input type='checkbox' />
+            <label>логотип</label>
+            <input type='checkbox' />
+            <label>фирменные цвета</label>
+            <input type='checkbox' />
+            <label>индивидуальные иконки</label>
+            <input type='checkbox' />
+            <label>индивидуальные картинки</label>
+            <input type='checkbox' />
+            <label>срочно</label>
+            <input type='checkbox' />
           </div>
         )}
       </div>
@@ -85,75 +110,3 @@ function ShopPage() {
 }
 
 export default ShopPage;
-
-// import React, { useState } from 'react';
-// import Header from '../../components/header/header';
-// import style from './shopPage.module.scss';
-
-// function ShopPage() {
-
-//   return (
-//     <>
-//       <Header />
-//       <div className={style.shop_page__wrapper}>
-//         <h1 className={style.shop_page__title}>магазин</h1>
-//         <h2>выберите :</h2>
-
-//         <div className={style.check_first}>
-//           <div className={style.site_blok_wrapper}>
-//             <label>сайты</label>
-//             <input
-//               type='radio'
-//               name='siteType'
-//               value='сайты'
-//             />
-//           </div>
-//         </div>
-
-//           <div className={style.site_seconde_blok_wrapper}>
-//           <label>лендинг</label>
-//           <input
-//             type='radio'
-//             name='siteTypeSeconde'
-//             value='лендинг'
-//             checked={selectedSiteSecondeType === 'лендинг'}
-//           />
-//           <label>многостраничный</label>
-//           <input
-//             type='radio'
-//             name='siteTypeSeconde'
-//             value='многостраничный'
-//             checked={selectedSiteSecondeType === 'многостраничный'}
-//           />
-//           <label>магазин</label>
-//           <input
-//             type='radio'
-//             name='siteTypeSeconde'
-//             value='магазин'
-//             checked={selectedSiteSecondeType === 'магазин'}
-//           />
-//         </div>
-
-//           
-
-//           <div className={style.site_fourth_blok_wrapper}>
-//             <label>дизайн</label>
-//             <input type='checkbox' />
-//             <label>логотип</label>
-//             <input type='checkbox' />
-//             <label>фирменные цвета</label>
-//             <input type='checkbox' />
-//             <label>индивидуальные иконки</label>
-//             <input type='checkbox' />
-//             <label>индивидуальные картинки</label>
-//             <input type='checkbox' />
-//             <label>срочно</label>
-//             <input type='checkbox' />
-//           </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default ShopPage;
-
