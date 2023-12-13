f73592827c2c805848cac34c15ce9bd07b092b46
Grant Kirill <webdotg.inbox@gmail.com>
2023-12-13 20:00:43 +0300

    white list

--- CLIENT/src/paths.ts
+++
@@ -1,8 +0,0 @@
-export const Paths = {
-  home :       '/',
-  register :   '/register',
-  login :      '/login',
-  portfolio :  '/portfolio',
-  shop :       '/shop',
-  cart :       '/cart',
-} as const
