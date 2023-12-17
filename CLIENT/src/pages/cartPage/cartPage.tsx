b7cd0e34b85ab1ab29a4b1f87bf38cdca3f42e7e
Grant Kirill <webdotg.inbox@gmail.com>
2023-12-17 22:43:21 +0300

    base style cart page

--- CLIENT/src/pages/cartPage/cartPage.tsx
+++ CLIENT/src/pages/cartPage/cartPage.tsx
@@ -1,13 +1,51 @@
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
-
+import style from './cartPage.module.scss'
 
 function CartPage() {
   return (
 
     <div>
       <Header />
-      cartPage
+      <section className={style["cart"]}>
+      <h2>Ваш заказ:</h2>
+      <ul className={style["cart-items"]}>
+        <li className={style["cart-item"]}>
+          <span className={style["item-name"]}>Название товара</span>
+          <span className={style["item-price"]}>$10</span>
+          <div className={style["quantity"]}>
+            <button className={style["dec-btn"]}>-</button>
+            <span className={style["item-quantity"]}>1</span>
+            <button className={style["inc-btn"]}>+</button>
+          </div>
+          <button className={style["remove-btn"]}>Удалить</button>
+        </li>
+        <li className={style["cart-item"]}>
+          <span className={style["item-name"]}>Название товара</span>
+          <span className={style["item-price"]}>$10</span>
+          <div className={style["quantity"]}>
+            <button className={style["dec-btn"]}>-</button>
+            <span className={style["item-quantity"]}>1</span>
+            <button className={style["inc-btn"]}>+</button>
+          </div>
+          <button className={style["remove-btn"]}>Удалить</button>
+        </li>
+        <li className={style["cart-item"]}>
+          <span className={style["item-name"]}>Название товара</span>
+          <span className={style["item-price"]}>$10</span>
+          <div className={style["quantity"]}>
+            <button className={style["dec-btn"]}>-</button>
+            <span className={style["item-quantity"]}>1</span>
+            <button className={style["inc-btn"]}>+</button>
+          </div>
+          <button className={style["remove-btn"]}>Удалить</button>
+        </li>
+      </ul>
+      <div className={style["cart-total"]}>
+        <p>Итого: <span className={style["total-amount"]}>$50</span></p>
+        <button className={style["checkout-btn"]}>Оформить заказ</button>
+      </div>
+    </section>
       <Footer />
     </div>
   )
--- CLIENT/src/pages/portfolioPage/portfolioPage.tsx
+++ CLIENT/src/pages/portfolioPage/portfolioPage.tsx
@@ -1,11 +1,29 @@
 
 import Footer from '../../components/footer/footer'
 import Header from '../../components/header/header'
+import style from './portfolioPage.module.scss'
+
 
 function PortfolioPage() {
+
   return (
     <div>
       <Header />
+      <section className={style["portfolio"]}>
+        <h2>Мои работы</h2>
+        <ul className={style["portfolio-list"]}>
+          <li className={style["portfolio-item"]}>
+            <img src="work1.jpg" alt="Работа 1" />
+            <h3>Название работы 1</h3>
+            <p>Описание работы 1</p>
+          </li>
+          <li className={style["portfolio-item"]}>
+            <img src="work2.jpg" alt="Работа 2" />
+            <h3>Название работы 2</h3>
+            <p>Описание работы 2</p>
+          </li>
+        </ul>
+      </section>
       <Footer />
     </div>
   )
