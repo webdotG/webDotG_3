d72bbff786cbdd69b17aeca30eed77761ac7fe14
Grant Kirill <webdotg.inbox@gmail.com>
2023-12-18 23:42:51 +0300

    add base style for radio and checkbox

--- CLIENT/src/pages/shopPage/shopPage.tsx
+++ CLIENT/src/pages/shopPage/shopPage.tsx
@@ -14,14 +14,17 @@ export default function ShopPage() {
             <span className={style['first-wrapper']}>
               <label htmlFor='websites'>сайты</label>
               <input className={style['radio']} id='websites' type='radio' name='first-radio' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
               <label htmlFor='telegramBot'>телеграм бот</label>
               <input className={style['radio']} id='telegramBot' type='radio' name='first-radio' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
               <label htmlFor='applications'>приложения</label>
               <input className={style['radio']} id='applications' type='radio' name='first-radio' />
+              <span className={style['custom-check']}></span>
             </span>
           </div>
 
@@ -29,66 +32,79 @@ export default function ShopPage() {
             <span className={style['first-wrapper']}>
               <label htmlFor='pattern'>шаблон</label>
               <input className={style['radio']} id='pattern' type='radio' name='seconde-radio' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
               <label htmlFor='custom'>индивидуальный</label>
-              <input className={style['radio']} id='custom' type='radio' name='second-radio' />
+              <input className={style['radio']} id='custom' type='radio' name='seconde-radio' />
+              <span className={style['custom-check']}></span>
             </span>
           </div>
 
           <div className={style['first-site-wrapper']}>
             <span className={style['first-wrapper']}>
               <label htmlFor='lending'>лендинг</label>
-              <input className={style['radio']} id='lending' type='radio' name='first-site-radio' />
+              <input className={style['radio']} id='lending' type='radio' name='site-radio' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
               <label htmlFor='multipage'>многостраничный</label>
-              <input className={style['radio']} id='multipage' type='radio' name='first-site-radio' />
+              <input className={style['radio']} id='multipage' type='radio' name='site-radio' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
               <label htmlFor='shop'>магазин</label>
-              <input className={style['radio']} id='shop' type='radio' name='first-site-radio' />
+              <input className={style['radio']} id='shop' type='radio' name='site-radio' />
+              <span className={style['custom-check']}></span>
             </span>
           </div>
 
           <div className={style['first-telegram-wrapper']}>
             <span className={style['first-wrapper']}>
               <label htmlFor='chat-bot'>чат бот</label>
-              <input className={style['radio']} id='chat-bot' type='radio' name='first-telegram-radio' />
+              <input className={style['radio']} id='chat-bot' type='radio' name='telegram-radio' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
               <label htmlFor='shop-bot'>магазин бот</label>
-              <input className={style['radio']} id='shop-bot' type='radio' name='first-telegram-radio' />
+              <input className={style['radio']} id='shop-bot' type='radio' name='telegram-radio' />
+              <span className={style['custom-check']}></span>
             </span>
           </div>
 
           <div className={style['first-app-wrapper']}>
             <span className={style['first-wrapper']}>
               <label htmlFor='i&a'>IOS & Android</label>
-              <input className={style['radio']} id='i&a' type='radio' name='first-app-radio' />
+              <input className={style['radio']} id='i&a' type='radio' name='app-radio' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
               <label htmlFor='vkapp'>VK App</label>
-              <input className={style['radio']} id='vkapp' type='radio' name='first-app-radio' />
+              <input className={style['radio']} id='vkapp' type='radio' name='app-radio' />
+              <span className={style['custom-check']}></span>
             </span>
           </div>
 
           <div className={style['additional-wrapper']}>
             <span className={style['first-wrapper']}>
-              <label htmlFor='lending'>хочу графику от иллюстратора</label>
-              <input className={style['checkbox']} id='lending' type='checkbox' name='first-site-radio' />
+              <label htmlFor='additional-check1'>графику от иллюстратора</label>
+              <input className={style['checkbox']} id='additional-check1' type='checkbox' name='additional-first' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
-              <label htmlFor='lending'>хочу супер крутые иконки</label>
-              <input className={style['checkbox']} id='lending' type='checkbox' name='first-site-radio' />
+              <label htmlFor='additional-check2'>индивидуальные иконки</label>
+              <input className={style['checkbox']} id='additional-check2' type='checkbox' name='additional-seconde' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
-              <label htmlFor='lending'>нужны крутые фотографии</label>
-              <input className={style['checkbox']} id='lending' type='checkbox' name='first-site-radio' />
+              <label htmlFor='additional-check3'>профессиональные фотографии</label>
+              <input className={style['checkbox']} id='additional-check3' type='checkbox' name='additional-therd' />
+              <span className={style['custom-check']}></span>
             </span>
             <span className={style['first-wrapper']}>
-              <label htmlFor='multipage'>срочно! нужно было еще вчера </label>
-              <input className={style['checkbox']} id='multipage' type='checkbox' name='first-site-radio' />
+              <label htmlFor='additional-check4'>СРОЧНО !!!</label>
+              <input className={style['checkbox']} id='additional-check4' type='checkbox' name='additional-fourth' />
+              <span className={style['custom-check']}></span>
             </span>
 
           </div>
