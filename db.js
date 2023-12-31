fe599e4034ecade941c7ece540bb0ac6f8d7bfc3
Grant Kirill <webdotg.inbox@gmail.com>
2023-12-31 11:09:32 +0300

    add env file env VITE_ and import.mets db.js

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ require('dotenv').config(); // Подключаем пакет для чтени
 // console.log('POOL DB JS SERVER : ', process.env)
 
 const pool = new Pool({
-  user: 'kirill',//process.env.USER,
-  host: '178.67.245.227',//process.env.HOST,
-  database: 'kyrill',//process.env.DATABASE,
-  password: 'KirillGrantWebDotG250125',//process.env.PASSWORD,
-  port: 11002//process.env.PORT,
+  user: import.meta.env.VITE_USER,
+  host: import.meta.env.VITE_HOST,
+  database: import.meta.env.VITE_DATABASE,
+  password: import.meta.env.VITE_PASSWORD,
+  port: parseInt(import.meta.env.VITE_PORT)
 })
 
 pool.query('SELECT NOW()').then() //x => console.log(x)
