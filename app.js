04615049bf3d7c708c7ae0a0350299929168569b
Grant Kirill <webdotg.inbox@gmail.com>
2024-01-06 23:24:27 +0300

    WORKING VERSION after five hours fck

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api/user', require('./routes/user'));
 
 app.use('/api/cart', require('./routes/cart'));
 
-const PORT = process.env.PORT; 
+const PORT = 1111 //process.env.PORT; 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 
