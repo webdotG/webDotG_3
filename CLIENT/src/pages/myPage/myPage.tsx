8aa9a883bffa302a9cdb0267e149cc60b2ca485f
Grant Kirill <webdotg.inbox@gmail.com>
2024-01-07 14:26:53 +0300

    fix myPage module.scss

--- CLIENT/src/pages/myPage/myPage.tsx
+++ CLIENT/src/pages/myPage/myPage.tsx
@@ -1,29 +1,17 @@
-import React, { useState } from 'react';
-import styles from './MyPage.module.scss';
+import React from 'react';
+import style from './myPage.module.scss';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 
 const MyPage: React.FC = () => {
-  const [selectedOption, setSelectedOption] = useState<string>('');
-
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(event.target.value);
-  };
 
   return (
-    <div className={styles.myPage}>
+    <div className={style['page-container']}>
       <Header />
-      <h2>Личный кабинет</h2>
-      <p>Добро пожаловать в ваш личный кабинет!</p>
-      <div className={styles.specialOption}>
-        <h3 className={styles.optionHeader}>Special Option</h3>
-        <select className={styles.select} onChange={handleSelectChange} value={selectedOption}>
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">чат</option>
-        </select>
+      <div className={style['mypage-wrapper']}>
+        <h2>Личный кабинет</h2>
+        <p>Добро пожаловать в ваш личный кабинет!</p>
       </div>
-      {/* {selectedOption === 'option3' && <Chat />} */}
       <Footer />
     </div>
   );
