afee4cff6a03d9a503d27928d88a2dee356578ab
Grant Kirill <webdotg.inbox@gmail.com>
2024-01-09 19:59:21 +0300

    deploy

--- CLIENT/src/support_function/is_error_with_message.ts
+++ CLIENT/src/support_function/is_error_with_message.ts
@@ -11,16 +11,3 @@ export const isErrorWithMessage = (error: unknown): error is typeErrorWithMessag
   )
 }
 
-// export type typeErrorWithMessage = {
-//   status: number;
-//   data: { message: string };
-// };
-
-// export const isErrorWithMessage = (error: unknown): error is typeErrorWithMessage => {
-//   return (
-//     typeof error === 'object' &&
-//     error !== null &&
-//     'data' in error &&
-//     typeof (error as Record<string, unknown>).data === 'object'
-//   );
-// };
