f613fc94ebb31100991e7f8bd13cd4c8d118a8f9
Grant Kirill <webdotg.inbox@gmail.com>
2024-01-19 02:18:26 +0300

    test ssh new

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './CLIENT/dist/index.html'));
 });
 
-const PORT = process.env.PORT  //1111; 
+const PORT = 1111 //process.env.PORT  ; 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 
