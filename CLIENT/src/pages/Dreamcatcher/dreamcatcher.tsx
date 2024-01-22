6755a46a1203f02028b0f2e61cd7d7c7333b1e0a
Grant Kirill <webdotg.inbox@gmail.com>
2024-01-22 18:56:08 +0300

    change AXIOS URL prod-dev

---
+++ CLIENT/src/pages/Dreamcatcher/dreamcatcher.tsx
@@ -0,0 +1,19 @@
+import Footer from '../../components/footer/footer'
+import Header from '../../components/header/header'
+import style from './dreamcatcher.module.scss'
+
+export default function Dreamcatcher() {
+  return (
+    <div className={style['page-container']}>
+      <Header />
+
+      <div className={style['dreamcatcher']}>
+
+    
+
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
