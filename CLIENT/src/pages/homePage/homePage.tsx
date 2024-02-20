96df7bd76a917addf7482a3ff2261b9f35f22a9a
Grant Kirill <webdotg.inbox@gmail.com>
2024-02-21 02:27:01 +0300

    portfolioMainPage new design

--- CLIENT/src/pages/homePage/homePage.tsx
+++ CLIENT/src/pages/homePage/homePage.tsx
@@ -1,11 +1,12 @@
 import style from './homePage.module.scss'
 import MainPriceBlokck from "../../components/mainPriceBlock/mainPriceBlokck";
-import MainPortfolioBock from "../../components/mainPortfolioBlock/mainPortfolioBock";
+// import MainPortfolioBock from "../../components/mainPortfolioBlock/mainPortfolioBock";
 import MainAboutBlok from "../../components/mainAboutBlok/mainAboutBlok";
 import MainFeedback from "../../components/mainFeedback/mainFeedback";
 import MainOrder from "../../components/mainOreder/mainOrder";
 import MainTitle from '../../components/mainTitle/mainTitle';
 import Paralax from '../../components/paralax/paralax';
+import MainPortfolio from '../../components/mainPortfolioBlock/mainPortfolio';
 // import ParalaxZoom from '../../components/paralax/paralaxZoom/paralaxZoom';
 
 function HomePage() {
@@ -17,7 +18,8 @@ function HomePage() {
       <MainPriceBlokck/>
       {/* <ParalaxZoom /> */}
       <Paralax type={'portfolio'} />
-      <MainPortfolioBock />
+      <MainPortfolio />
+      {/* <MainPortfolioBock /> */}
       <MainAboutBlok />
       {/* <MainFeedback /> */}
       {/* <MainOrder /> */}
