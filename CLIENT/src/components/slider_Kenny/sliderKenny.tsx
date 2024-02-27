1bc8bcae70b362684b6a443dfe0c48a1a8244ea4
Grant Kirill <webdotg.inbox@gmail.com>
2024-02-27 07:08:53 +0300

    KennySlider img style

--- CLIENT/src/components/slider_Kenny/sliderKenny.tsx
+++ CLIENT/src/components/slider_Kenny/sliderKenny.tsx
@@ -17,19 +17,54 @@ export default function SliderKenny() {
   return (
     <section className={style['sliderKenny-wrapper']} >
       <div className={style['sliderKenny']}>
+
         <ul className={style['sliderKenny-list']}>
-              <li className={style['sliderKenny-item-chees']} >
-                <img src={CheesingKenny} alt='CheesingKenny' />
-              </li>
-              <li className={style['sliderKenny-item-batman']} >
-                <img src={BatmanKenny} alt='BatmanKenny' />
-              </li>
-              <li className={style['sliderKenny-item-princess']} >
-                <img src={PrincesseKenny} alt='PrincesseKenny' />
-              </li>
-              <li className={style['sliderKenny-item-mysterion']} >
-                <img src={MysterionKenny} alt='MysterionKenny' />
-              </li>
+          <li className={style['sliderKenny-item-chees']} >
+            <h2>WEB</h2>
+            
+            <section>
+              <table>
+                <thead>
+                  <>
+                    <th>Лэндинг:</th>
+                    <th>Плотфолио:</th>
+                    <th>Магазин:</th>
+                  </>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                  </tr>
+                  <tr>
+                    <td>7</td>
+                    <td>8</td>
+                    <td>9</td>
+                  </tr>
+                </tbody>
+              </table>
+
+            </section>
+            <img src={CheesingKenny} alt='CheesingKenny' />
+          </li>
+          <li className={style['sliderKenny-item-batman']} >
+            <h2>Bot</h2>
+            <img src={BatmanKenny} alt='BatmanKenny' />
+          </li>
+          <li className={style['sliderKenny-item-princess']} >
+            <h2>App</h2>
+            <img src={PrincesseKenny} alt='PrincesseKenny' />
+          </li>
+          <li className={style['sliderKenny-item-mysterion']} >
+            <h2>NPM</h2>
+            <img src={MysterionKenny} alt='MysterionKenny' />
+          </li>
         </ul>
       </div>
     </section>
