caad22698c2d38e401c66c0418b3eb45f624751f
Grant Kirill <webdotg.inbox@gmail.com>
2024-02-29 05:20:26 +0300

    main  portfolio text style add base

--- CLIENT/src/components/mainPortfolioBlock/mainPortfolio.tsx
+++ CLIENT/src/components/mainPortfolioBlock/mainPortfolio.tsx
@@ -19,64 +19,72 @@ import { useScroll, useTransform, motion } from 'framer-motion'
 export default function MainPortfolio() {
   const ref = useRef<HTMLElement>(null)
 
-  const {scrollYProgress} = useScroll({
+  const { scrollYProgress } = useScroll({
     target: ref,
     offset: ['start end', 'end start']
   })
 
-  const small = useTransform(scrollYProgress, [0,1], [0, -50])
-  const medium = useTransform(scrollYProgress, [0,1], [0, -150])
-  const large = useTransform(scrollYProgress, [0,1], [0, -350])
+  const small = useTransform(scrollYProgress, [0, 1], [0, -50])
+  const medium = useTransform(scrollYProgress, [0, 1], [0, -150])
+  const large = useTransform(scrollYProgress, [0, 1], [0, -350])
 
   return (
     <section ref={ref} className={style['mainPortfolio-container']}>
-      
+
       <div className={style['wrapper']}>
         <div className={style['word']}>
-          <h3>Сайт бронирования отелей</h3>
-          <p>🌍 Глобальное покрытие: можете выбирать и бронировать отели в любой точке земного шара</p>
-          <p>💼 Удобство в бронировании: интуитивно понятный интерфейс позволяет выбирать, сравнивать цены и отели</p>
-          <p>💳 Безопасная оплата:различные варианты оплаты</p>
+          <h3>UI</h3>
+          <p>от Мобилки до Десктопа  </p>
+          <p>Понятный интерфейс,<br/>
+           тренды дизайна,<br/>
+           анимация</p>
+          <p>
+            Лендинг,<br />
+            Внутренняя программа,<br />
+            Кинотеатр,<br />
+            Магазин.
+          </p>
         </div>
         <div className={style['images']}>
-          <motion.img style={{y: medium}} className={style['booking-listing']} src={listing} alt='project image' height='300px' width='100px' />
-          <motion.img style={{y: small}} className={style['booking-home']} src={home} alt='project image' height='550px' width='225px' />
-          <motion.img style={{y: large}} className={style['booking-profile']} src={profile} alt='project image' height='250px' width='200px'/>
+          <motion.img style={{ y: medium }} className={style['booking-listing']} src={listing} alt='project image' height='300px' width='100px' />
+          <motion.img style={{ y: small }} className={style['booking-home']} src={home} alt='project image' height='550px' width='225px' />
+          <motion.img style={{ y: large }} className={style['booking-profile']} src={profile} alt='project image' height='250px' width='200px' />
         </div>
       </div>
-    
+
       <div className={style['wrapper']}>
         <div className={style['images']}>
-          <motion.img style={{y: large}} className={style['telegram-chat']} src={telegramChat} alt='project image' height='400px' width='200px' />
-          <motion.img style={{y: small}} className={style['telegram-desctop']} src={telegramDesctop} alt='project image' height='300px' width='400px' />
-          <motion.img style={{y: medium}} className={style['telegram-call']} src={telegramCall} alt='project image' height='250px' width='175px'/>
+          <motion.img style={{ y: large }} className={style['telegram-chat']} src={telegramChat} alt='project image' height='400px' width='200px' />
+          <motion.img style={{ y: small }} className={style['telegram-desctop']} src={telegramDesctop} alt='project image' height='300px' width='400px' />
+          <motion.img style={{ y: medium }} className={style['telegram-call']} src={telegramCall} alt='project image' height='250px' width='175px' />
         </div>
         <div className={style['word']}>
-          <h3>Бот</h3>
-          <p>Telegram бот будущее коммуникаций </p>
-          <p>🤖 Чат-бот: Умная связь, мгновенные ответы! экономите время От поддержки клиентов до заказа услуг</p>
-          <p>🛒 Магазин-бот: онлайн-шопинг </p>
-          <p>🚀 Преимущества:  Мгновенность  Персонализация Простота использования
-          </p>
+          <h3>Telegram</h3>
+          <p>Telegram бот это удобство коммуникаций </p>
+          <p>мгновенные ответы,<br/>
+          выигрывание времени на<br/>
+          поддержке клиентов и<br/>
+          скорости продаж.</p>
+          <p>Магазин-бот, Чат-бот ... </p>
         </div>
       </div>
-      
+
       <div className={style['wrapper']}>
-      <div className={style['word']}>
-          <h3>Мобильное приложение</h3>
-          <p> чат-интерфейс для сообщений или приложение для онлайн-шопинга</p>
-          <p>📱 Чат-приложение: Где бы вы ни были</p>
-          <p>🛍️ Приложение для шопинга: Весь магазин у вас в кармане!</p>
-          <p>🚀 Преимущества:  Мобильность  Персонализация  Простота использования
-          </p>
-         </div>
+        <div className={style['word']}>
+          <h3>App</h3>
+          <p>Чат, магазин, ежедневник ...</p>
+          <p>Встраивается в телефон</p>
+          <p>Доступ к геолокации,<br/>
+          камере,<br/>
+          ко всем функциям телефона.</p>
+        </div>
         <div className={style['images']}>
-          <motion.img style={{y: small}} className={style['mobile-loginLight']} src={mobileLoginLight} alt='project image' height='250px' width='125px' />
-          <motion.img style={{y: small}} className={style['mobile-LoginDark']} src={mobileLoginDark} alt='project image' height='250px' width='125px' />
-          <motion.img style={{y: large}} className={style['mobile-homeLight']} src={mobileHomeLight} alt='project image' height='250px' width='125px'/>
-          <motion.img style={{y: large}} className={style['mobile-homeDark']} src={mobileHomeDark} alt='project image' height='250px' width='125px' />
-          <motion.img style={{y: medium}} className={style['mobile-chatLight']} src={mobileChatLight} alt='project image' height='250px' width='125px' />
-          <motion.img style={{y: medium}} className={style['mobile-chatDark']} src={mobileChatDark} alt='project image' height='250px' width='125px'/>
+          <motion.img style={{ y: small }} className={style['mobile-loginLight']} src={mobileLoginLight} alt='project image' height='250px' width='125px' />
+          <motion.img style={{ y: small }} className={style['mobile-LoginDark']} src={mobileLoginDark} alt='project image' height='250px' width='125px' />
+          <motion.img style={{ y: large }} className={style['mobile-homeLight']} src={mobileHomeLight} alt='project image' height='250px' width='125px' />
+          <motion.img style={{ y: large }} className={style['mobile-homeDark']} src={mobileHomeDark} alt='project image' height='250px' width='125px' />
+          <motion.img style={{ y: medium }} className={style['mobile-chatLight']} src={mobileChatLight} alt='project image' height='250px' width='125px' />
+          <motion.img style={{ y: medium }} className={style['mobile-chatDark']} src={mobileChatDark} alt='project image' height='250px' width='125px' />
         </div>
       </div>
 
