236bd9ed015333246411aceaeb47492970e2392e
Grant Kirill <webdotg.inbox@gmail.com>
2024-02-29 07:57:13 +0300

    style fix any

--- CLIENT/src/components/slider_Kenny/sliderKenny.tsx
+++ CLIENT/src/components/slider_Kenny/sliderKenny.tsx
@@ -97,19 +97,19 @@ export default function SliderKenny() {
     setCurrentSlide(index);
   };
 
-  // const startSlideShow = () => {
-  //   return setInterval(() => {
-  //     nextSlide();
-  //   }, 3500);
-  // };
+  const startSlideShow = () => {
+    return setInterval(() => {
+      nextSlide();
+    }, 14500);
+  };
 
-  // useEffect(() => {
-  //   const intervalId = startSlideShow();
+  useEffect(() => {
+    const intervalId = startSlideShow();
 
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, []);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
 
   return (
