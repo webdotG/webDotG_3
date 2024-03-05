a8659d1f5c804c2bb1c8daa87e5e37f3ec26907c
Grant Kirill <webdotg.inbox@gmail.com>
2024-03-06 00:05:36 +0300

    login page btn-show password light grey

--- CLIENT/src/pages/loginPage/loginPage.tsx
+++ CLIENT/src/pages/loginPage/loginPage.tsx
@@ -110,13 +110,14 @@ const LoginPage: React.FC = () => {
               />
               <button className={styles['btn-show-password']} type="button" onClick={handleTogglePassword}>
                 {showPassword
-                  ? (<svg fill="none" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4.5 8C7.5 14.5 16.5 14.5 19.5 8" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-                    <path d="M16.8162 11.3175L19.5 15" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-                    <path d="M12 12.875V16.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-                    <path d="M7.18383 11.3175L4.5 15" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+                  ? (<svg className={styles['open-eye']} fill='none' strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.5 8C7.5 14.5 16.5 14.5 19.5 8"  strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M16.8162 11.3175L19.5 15"  strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M12 12.875V16.5"  strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M7.18383 11.3175L4.5 15"  strokeLinecap="round" strokeLinejoin="round" />
                   </svg>)
                   : (<svg
+                    className={styles['close-eye']}
                     id="Layer_1"
                     version="1.1"
                     viewBox="0 0 80 80"
