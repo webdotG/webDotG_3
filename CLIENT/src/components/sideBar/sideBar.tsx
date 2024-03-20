48202dbb942b30235b7adc7515ec9c55e745710f
Grant Kirill <webdotg.inbox@gmail.com>
2024-03-20 15:38:10 +0300

    last fix)))), deploy change planet2 background position

--- CLIENT/src/components/sideBar/sideBar.tsx
+++ CLIENT/src/components/sideBar/sideBar.tsx
@@ -1,47 +1,58 @@
-  import { useState } from 'react'
-  import { motion } from 'framer-motion'
-  import Links from './Links/links'
-  import ToggleButton from './ToggleButton/toggleButton'
-  import style from './sideBar.module.scss'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import Links from './Links/links'
+import ToggleButton from './ToggleButton/toggleButton'
+import style from './sideBar.module.scss'
 
-  export default function SideBar() {
+export default function SideBar() {
 
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-    const variants = {
-      open: {
-        clipPath: 'circle(1200px at 50px 50px)',
-        transition: {
-          duration: 1.25,
-        }
-      },
-      closed: {
-        clipPath: 'circle(30px at 50px 50px)',
-        transition: {
-          delay: 0.25,
-          type: 'spring',
-          stiffness: 150,
-          damping: 40,
-          duration: 0.5,
-        }
+  const variants = {
+    open: {
+      clipPath: 'circle(1200px at 50px 50px)',
+      transition: {
+        duration: 1.25,
+      }
+    },
+    closed: {
+      clipPath: 'circle(30px at 50px 50px)',
+      transition: {
+        delay: 0.25,
+        type: 'spring',
+        stiffness: 150,
+        damping: 40,
+        duration: 0.5,
       }
     }
-
-    const closeSidebar = () => {
-      setOpen(false);
-    };
-
-    return (
-      <motion.div
-        className={style['sideBar']}
-        animate={open ? 'open' : 'closed'}>
-        <motion.div
-          className={style['backGround']}
-          variants={variants}
-        >
-          <Links closeSidebar={closeSidebar} />
-        </motion.div>
-        <ToggleButton setOpen={setOpen} />
-      </motion.div>
-    )
   }
+
+  const closeSidebar = () => {
+    setOpen(false);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (!event.target.closest(`.${style['sideBar']}`)) {
+      closeSidebar();
+    }
+  };
+
+  return (
+    <motion.div
+      className={style['sideBar']}
+      animate={open ? 'open' : 'closed'}
+      onClick={handleOutsideClick}
+    >
+      <motion.div
+        className={style['backGround']}
+        variants={variants}
+        initial="closed"
+        animate={open ? 'open' : 'closed'}
+        exit="closed"
+      >
+        <Links closeSidebar={closeSidebar} />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  )
+}
